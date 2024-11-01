968d4b252f36eea3d7824cd3dc35113d18356856
Diego <diegoneira@gmail.com>
2024-10-31 21:52:55 -0300

    dia1

---
+++ Curso holamundo/Tipos avanzados/01-listas.py
@@ -0,0 +1,10 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["feo", "ql"]
+maspalabras = ["feo", "ql", "te", "quiero", "mucho"]
+booleans = [True, False, False]
+matriz = [[0, 1], [1, 0]]
+ceros = [0, 1] * 10
+alfanumerico = numeros + letras
+rango = list(range(1, 11))
+chars = list("hola mundo")
